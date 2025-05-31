a60064e6da5df097ec36bcbb0b17f6b7c6152711
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-05-31 18:34:25 -0300

    Commit

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 
 <?php
-//iniciar sessao
 
-//não mostrar ERROS 
 error_reporting(~E_ALL & ~E_NOTICE & ~E_WARNING);
 
 
-// Ativa o carregamento automático das classes
 require_once 'autoload.php';
 
-// Inicia a sessão (caso o sistema use autenticação)
 session_start();
 
-// Define um roteador básico para direcionar páginas
 require_once 'router.php';
